41bd353364570d32d2729a7e21a15e44840125f4
Harris Nguyen <hainx1610@gmail.com>
2022-10-11 20:08:21 +0700

    finish exercise 02-repeatstring

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = (string, num) => {
+    let result = "";
+    if (num < 0) return "ERROR";
+    for (let i = 0; i < num; i++) {
+       result += string; 
+    }
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
