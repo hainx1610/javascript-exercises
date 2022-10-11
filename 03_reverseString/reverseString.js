84ce733250b1582d0871c8c3de5031b86dcb540e
Harris Nguyen <hainx1610@gmail.com>
2022-10-11 21:56:34 +0700

    finish exercise 03-reversestring

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = (inputString) => {
+    let reverseString = "";
+    for (let i = inputString.length; i >0; i--) {
+        reverseString += inputString[i - 1];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
