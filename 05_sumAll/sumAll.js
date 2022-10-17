c76d237a2f76bbf6095c407cb98e9534c900a93f
Harris Nguyen <hainx1610@gmail.com>
2022-10-17 19:16:15 +0700

    finish exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = (a, b) => {
+    let total = 0;
+    if ((typeof a != "number") || (typeof b != "number")) {
+        total = "ERROR";
+    } else {
+        if ((a < 0)||(b < 0)) {
+            total = "ERROR";
+        } else {
+            if (a < b) {
+                for (let i = a; i <= b; i++) {
+                    total += i;
+                }
+            } else {
+                for (let i = b; i <= a; i++) {
+                    total += i;
+                }
+            }
+        }
+    }
+    
+    
+    return total;
 };
 
 // Do not edit below this line
