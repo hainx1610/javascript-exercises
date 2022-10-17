4e0e20f399fb080ef90c967baadea4379f441c96
Harris Nguyen <hainx1610@gmail.com>
2022-10-17 18:04:00 +0700

    finish exercise 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = (array, ...theArgs) => {
+    for (const arg of theArgs) {
+        for (const element of array) {
+            if (arg === element) {
+                array.splice(array.indexOf(element), 1);
+            }
+        }
+    }
+    
+    return array;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
