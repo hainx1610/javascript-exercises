c9b207fdb91dcb8b96237402048d15391f47c91c
Harris Nguyen <hainx1610@gmail.com>
2022-10-17 20:44:19 +0700

    finish exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
-};
+const leapYears = (year) => {
+    let result;
+    if (year % 4 === 0) {
+        if ((year % 100 === 0) && (year % 400 !== 0)) {
+            result = false;
+        } else {
+            result = true;
+        }
+    } else {
+        result = false;
+    };
+    return result;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
