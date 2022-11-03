98679807ffd3047af337f6e40451a70a5ab25006
Harris Nguyen <hainx1610@gmail.com>
2022-11-03 21:16:28 +0700

    finish exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((result, number) => result + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((result, number) => result *= number, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  if (n >= 1) {
+    for (let i = 1; i <= n; i++) {
+      result *= i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
