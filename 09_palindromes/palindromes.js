e39d277ec023a26317571748b00a50a405257406
Harris Nguyen <hainx1610@gmail.com>
2022-11-03 22:57:27 +0700

    finish exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+// const palindromes = function (str) {
+//     let processedStr = str.toLowerCase().replace(/[^A-Za-z0-9]/g, "")
+//     return processedStr == processedStr.split("").reverse().join("");
+// };
 
+//or use loop/half-indexing
+const palindromes = function (str) {
+    let processedStr = str.toLowerCase().replace(/[^A-Za-z0-9]/g, ""); //use regex to remove spaces and commas
+    let strLength = processedStr.length;
+    for (let i = 0; i < strLength/2; i++) {
+        if (processedStr[i] !== processedStr[strLength - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
