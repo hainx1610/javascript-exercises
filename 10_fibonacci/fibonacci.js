98525f2048283abedead641c03ec7938b8d5ef78
Harris Nguyen <hainx1610@gmail.com>
2022-11-04 10:35:39 +0700

    finish exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let t = +n; // to accept strings as parameters
+    let sequence = [1, 1];
+    let result;
+    if (t === 1) {
+        result = 1;
+    } else if (t === 2) {
+        result = 1;
+    } else if (t > 2) {
+        for (let i = 3; i <= t; i++) {
+            let value = sequence[i - 3] + sequence[i - 2]; // index = t - 1
+            sequence.push(value);
+        }
+        result = sequence[t - 1];
+    } else {
+        result = "OOPS";
+    }
+    return result;
 };
 
 // Do not edit below this line
