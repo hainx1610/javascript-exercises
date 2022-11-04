d8e0cbf8ffb16690af05fcf78418d0c5522fce06
Harris Nguyen <hainx1610@gmail.com>
2022-11-04 13:50:32 +0700

    finish exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(list) {
+    const sorted = getAgeList(list).sort((a, b) => a.age > b.age ? -1 : 1);
+    return sorted[0];
+}
 
-};
+const getAgeList = list => {
+    const array = [];
+    for (const person of list) {
+        let age = 0;
+        if (person.yearOfDeath) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            age = (new Date()).getFullYear() - person.yearOfBirth;
+        }
+        array.push({
+            name: person.name,
+            age: age
+        });
+    };
+    return array;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
